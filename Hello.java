eb3e35dec306e0949a681616c8397bb21b5d53ba
Kanchanawanchai <tkanchanawanchai6403@email.vccs.edu>
2016-10-13 15:51:11 -0400

    add Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
 	public static void main(String [] args) {
 		System.out.println("Hello from Australia by CSC202");
+		System.out.println("Hello from Australia by CSC202 second change");
 	}
 }
